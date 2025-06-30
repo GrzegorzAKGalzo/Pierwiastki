967a19f9dd8c24a5f58cc1af61f910854fe8ecee
Grzegorz <grzeslach8@gmail.com>
2025-06-30 03:06:26 +0200

    feat: edit table values popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ELEMENT_DATA, PeriodicElement } from './periodic-element';
+import { MatDialog } from '@angular/material/dialog';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,26 @@ export class AppComponent {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource: PeriodicElement[] = [];
 
- ngOnInit(): void {
-    // Symulacja pobierania danych
-    setTimeout(() => {
-      this.dataSource = [...ELEMENT_DATA];
-    }, 1000);
+  constructor(private dialog: MatDialog){}
 
-   
+  ngOnInit(): void {
+      // Symulacja pobierania danych
+      setTimeout(() => {
+        this.dataSource = [...ELEMENT_DATA];
+      }, 1000);
+    }
+
+
+    openEditDialog<K extends keyof PeriodicElement>(element: PeriodicElement, column: K): void {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      data: { value: element[column] }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result !== undefined) {
+        element[column] = result as PeriodicElement[K]; // 👈 tu rzutowanie
+        this.dataSource = [...this.dataSource];
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatTableModule} from '@angular/material/table'
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
